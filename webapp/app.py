d8a6b2e193ce4a4da7e30a12b47294a8c3b29ed6
ksmurakoshi <ksmurakoshi@ucdavis.edu>
2016-05-06 14:41:09 -0700

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    provider = str(os.environ.get('PROVIDER', 'world'))
-    return 'Goodbye '+provider+'!'
+    provider = str(os.environ.get('PROVIDER', 'moon'))
+    return 'Goodnight '+provider+'!'
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
